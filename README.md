4911635ba121900f5aa9d316b0011bd33203a9ee
souad988 <59707859+souad988@users.noreply.github.com>
2022-02-17 20:33:25 +0100

    Add styling to my todo list

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# webPack
-webPack setup
+# ToDoList
+# AwsomeBooksModules
+
+Javascript App that keeps track of awesome books
+
+## Built With
+
+-HTML & Javascript
+
+## Getting Started
+
+**To get a local copy up and running follow these simple example steps.**
+
+Clone the repo with `git clone git@github.com:souad988/AwsomeBooksModules.git`
+
+Run `npm install` from the command line
+
+Run `npx hint .` to test for HTML linting check
+
+Run `npx stylelint "**/*.{css,scss}"` to test for css/scss linting check
+
+Run `npx eslint .` to test for javascript linting check
+
+The app is deployed using Github Pages. You can view it by clicking the [Live Demo Link](#Live-Demo) below.
+### Live demo
+
+[gitpages](https://souad988.github.io/AwsomeBooksModules/)
+
+## Authors
+
+👤 **Souad**
+
+- GitHub: [@githubhandle](https://github.com/souad988)
+  https://github.com/souad988
+
+## Screenshot ![see](./images/screenshot.png?raw=true "Title")
+
+Screenshot:
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the (https://github.com/souad988/AwsomeBooksModules/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+- Microverse
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
+
+
--- src/index.js
+++ src/index.js
@@ -13,12 +13,12 @@ let todolist = [
   },
   {description :'exercice',
   completed :false,
-  index: 0,
+  index: 2,
   },
   {
     description :'do homeWork',
     completed :true,
-    index: 1,
+    index: 3,
   },
 ];
 
@@ -26,13 +26,34 @@ function displayToDoTasks(todolist,tasks) {
   tasks.innerHTML =``;
   // Lodash, now imported by this script
   todolist.forEach(task => {
-    tasks.innerHTML +=`<li><form class="task">
+    tasks.innerHTML +=`<li class="task_row"><form class="task">
     <input type="checkbox" id="${task.index}" name="task" value="task">
     <label for="${task.index}">${task.description}</label>
-    <i class="fas fa-trash-alt"></i>
+    <i class="fas fa-trash-alt hide" ></i>
+    <i class="fas fa-ellipsis-v show"></i>
     </form></li>`;
   });
 }
 
 const tasks = document.querySelector('.tasks');
 displayToDoTasks(todolist,tasks);
+
+tasks.addEventListener('click', (e) => {
+    const dotIconList= document.querySelectorAll('.fa-ellipsis-v');
+    const trushIconList= document.querySelectorAll('.fa-trash-alt');
+    dotIconList.forEach(dotIcon=>{dotIcon.classList.remove('hide');
+    dotIcon.classList.add('show');
+    })
+    trushIconList.forEach(trushIcon=>{trushIcon.classList.remove('show');
+    trushIcon.classList.add('hide');
+    })
+    //dotsList=e.path.filter(item=>)
+    const dots = e.target.querySelector('.fa-ellipsis-v');
+    const trush = e.target.querySelector('.fa-trash-alt');
+     trush.classList.remove('hide');
+     dots.classList.remove('show');
+     dots.classList.add('hide');
+     trush.classList.add('show');
+     console.log('entered');
+});
+
