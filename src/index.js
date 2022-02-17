24e7ef540bfa6eddf855073af69ad120ccf20a31
souad988 <59707859+souad988@users.noreply.github.com>
2022-02-17 20:53:57 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import _ from 'lodash';
+import _ from 'lodash';//eslint-disable-line
 import './style.css';
 
-let todolist = [
-  {description :'exercice',
-  completed :false,
-  index: 0,
+const todolist = [
+  {
+    description: 'exercice',
+    completed: false,
+    index: 0,
   },
   {
-    description :'do homeWork',
-    completed :true,
+    description: 'do homeWork',
+    completed: true,
     index: 1,
   },
-  {description :'exercice',
-  completed :false,
-  index: 2,
+  {
+    description: 'exercice',
+    completed: false,
+    index: 2,
   },
   {
-    description :'do homeWork',
-    completed :true,
+    description: 'do homeWork',
+    completed: true,
     index: 3,
   },
 ];
 
-function displayToDoTasks(todolist,tasks) {
-  tasks.innerHTML =``;
+function displayToDoTasks(todolist, tasks) {
+  tasks.innerHTML = '';
   // Lodash, now imported by this script
-  todolist.forEach(task => {
-    tasks.innerHTML +=`<li class="task_row"><form class="task">
+  todolist.forEach((task) => {
+    tasks.innerHTML += `<li class="task_row"><form class="task">
     <input type="checkbox" id="${task.index}" name="task" value="task">
     <label for="${task.index}">${task.description}</label>
     <i class="fas fa-trash-alt hide" ></i>
@@ -36,24 +38,25 @@ function displayToDoTasks(todolist,tasks) {
 }
 
 const tasks = document.querySelector('.tasks');
-displayToDoTasks(todolist,tasks);
+displayToDoTasks(todolist, tasks);
 
 tasks.addEventListener('click', (e) => {
-    const dotIconList= document.querySelectorAll('.fa-ellipsis-v');
-    const trushIconList= document.querySelectorAll('.fa-trash-alt');
-    dotIconList.forEach(dotIcon=>{dotIcon.classList.remove('hide');
+  const dotIconList = document.querySelectorAll('.fa-ellipsis-v');
+  const trushIconList = document.querySelectorAll('.fa-trash-alt');
+  dotIconList.forEach((dotIcon) => {
+    dotIcon.classList.remove('hide');
     dotIcon.classList.add('show');
-    })
-    trushIconList.forEach(trushIcon=>{trushIcon.classList.remove('show');
+  });
+  trushIconList.forEach((trushIcon) => {
+    trushIcon.classList.remove('show');
     trushIcon.classList.add('hide');
-    })
-    //dotsList=e.path.filter(item=>)
-    const dots = e.target.querySelector('.fa-ellipsis-v');
-    const trush = e.target.querySelector('.fa-trash-alt');
-     trush.classList.remove('hide');
-     dots.classList.remove('show');
-     dots.classList.add('hide');
-     trush.classList.add('show');
-     console.log('entered');
+  });
+  // dotsList=e.path.filter(item=>)
+  const dots = e.target.querySelector('.fa-ellipsis-v');
+  const trush = e.target.querySelector('.fa-trash-alt');
+  trush.classList.remove('hide');
+  dots.classList.remove('show');
+  dots.classList.add('hide');
+  trush.classList.add('show');
+  console.log('entered');
 });
-
