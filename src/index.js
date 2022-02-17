6818e405f2fe98df901b57ff6f5bf3a37daddc0c
souad988 <59707859+souad988@users.noreply.github.com>
2022-02-17 15:41:20 +0100

    Create tasklist and dispaly function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+let todolist = [
+  {description :'exercice',
+  completed :false,
+  index: 0,
+  },
+  {
+    description :'do homeWork',
+    completed :true,
+    index: 1,
+  },
+  {description :'exercice',
+  completed :false,
+  index: 0,
+  },
+  {
+    description :'do homeWork',
+    completed :true,
+    index: 1,
+  },
+];
 
+function displayToDoTasks(todolist,tasks) {
+  tasks.innerHTML =``;
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+  todolist.forEach(task => {
+    tasks.innerHTML +=`<li><form class="task">
+    <input type="checkbox" id="${task.index}" name="task" value="task">
+    <label for="${task.index}">${task.description}</label>
+    <i class="fas fa-trash-alt"></i>
+    </form></li>`;
+  });
 }
 
-document.body.appendChild(component());
+const tasks = document.querySelector('.tasks');
+displayToDoTasks(todolist,tasks);
