39140c184315403ce2b778a38ac4ddbfb68b8687
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-24 11:56:03 +0100

    Add remove all button functions

--- src/index.test.js
+++ src/index.test.js
@@ -16,6 +16,18 @@ const task = {
   completed: false,
 };
 
+// The same functions of the index.js just instead of importing it we created it here
+// to prevent importing the styles, which give an error.
+const editTaskDescription = (desc, id, todolist) => {
+  todolist.update(desc, id);
+  populateStorage(todolist.tasks);
+};
+
+const editTaskStatus = (id, todolist) => {
+  todolist.checkCompleted(id);
+  populateStorage(todolist.tasks);
+};
+
 test('add One Task to DOM using add() func', () => {
   myTodolist.add(task.description);
 
@@ -32,17 +44,6 @@ test('Remove One Task DOM using remove() func', () => {
   }
   expect(Body.innerHTML).toBe('');
 });
-
-//
-const editTaskDescription = (desc, id, todolist) => {
-  todolist.update(desc, id);
-  populateStorage(todolist.tasks);
-};
-
-const editTaskStatus = (id, todolist) => {
-  todolist.checkCompleted(id);
-  populateStorage(todolist.tasks);
-};
 
 describe('Test local storage', () => {
   test('Edit a Task description and check localstorage', () => {
@@ -64,5 +65,30 @@ describe('Test check status', () => {
   test('Check if the Task is checked (have value of true)', () => {
     editTaskStatus(1, myTodolist);
     expect(setInputs()[0].completed).toBe(true);
+  });
+});
+
+describe('Test clear all button', () => {
+  test('Should not remove task if checked', () => {
+    myTodolist.add(task.description);
+    myTodolist.add(task.description);
+    myTodolist.removeCompleted();
+    populateStorage(myTodolist.tasks);
+    if (myTodolist.tasks.length === 0) {
+      Body.innerHTML = '';
+    }
+    expect(Body.innerHTML).toBe('<li>desc</li>');
+  });
+
+  test('Should remove all the tasks if not checked', () => {
+    editTaskStatus(1, myTodolist);
+    myTodolist.add(task.description);
+    // myTodolist.add(task.description);
+    myTodolist.removeCompleted();
+    populateStorage(myTodolist.tasks);
+    if (myTodolist.tasks.length === 0) {
+      Body.innerHTML = '';
+    }
+    expect(Body.innerHTML).toBe('');
   });
 });
